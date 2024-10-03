2a53aee87bb37dda5b54922c1a3bb269bcb9e622
quochoangdev <quochoangdev.official@gmail.com>
2024-10-03 19:28:14 +0700

    update product

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -10,13 +10,13 @@ const readFunc = async (req, res) => {
       let { count, rows } = await db.Product.findAndCountAll({
         offset: offset,
         limit: limit,
-        attributes: ["id", "title", "product_id", "capacity_id", "color_id", "stock", "discount", "price", "desc", "slug", "category_id", "updatedAt", "createdAt"],
+        attributes: ["id", "title", "capacity_id", "color_id", "stock", "discount", "price", "desc", "slug", "category_id", "updatedAt", "createdAt"],
         order: [["title", "ASC"]],
       })
       const totalPages = Math.ceil(count / limit);
       data = { totalRows: count, totalPages: totalPages, product: rows, }
     } else {
-      data = await db.Product.findAll({ attributes: ["id", "title", "product_id", "capacity_id", "color_id", "stock", "discount", "price", "desc", "slug", "category_id", "updatedAt", "createdAt"], order: [["title", "ASC"]] })
+      data = await db.Product.findAll({ attributes: ["id", "title", "capacity_id", "color_id", "stock", "discount", "price", "desc", "slug", "category_id", "updatedAt", "createdAt"], order: [["title", "ASC"]] })
     }
     return res.status(200).json({ message: "get product success", code: 0, data: data, });
   } catch (error) {
@@ -27,13 +27,12 @@ const readFunc = async (req, res) => {
 
 const createFunc = async (req, res) => {
   try {
-    const { title, product_id, capacity_id, color_id, stock, discount, price, desc, category_id } = req.body.data;
+    const { title, capacity_id, color_id, stock, discount, price, desc, category_id } = req.body.data;
 
     let combinedString = `${title}-${capacity_id}-${category_id}`;
     let slug = slugify(combinedString, { lower: true, strict: true, replacement: '-' });
-
-    if (!title || !product_id || !capacity_id || !color_id || !stock || !discount || !price || !category_id) return res.status(200).json({ message: "missing required parameters", code: 1 });
-    let data = await db.Product.create({ title: title, product_id: product_id, capacity_id: capacity_id, color_id: color_id, stock: stock, discount: discount, price: price, desc: desc, category_id: category_id, slug: slug });
+    if (!title || !capacity_id || !color_id || !stock || !discount || !price || !category_id) return res.status(200).json({ message: "missing required parameters", code: 1 });
+    let data = await db.Product.create({ title: title, capacity_id: capacity_id, color_id: color_id, stock: stock, discount: discount, price: price, desc: desc, category_id: category_id, slug: slug });
     return res.status(200).json({ message: "a product is created successfully", code: 0, data: data });
   } catch (error) {
     return res.status(500).json({ message: "error from server", code: -1 });
@@ -54,8 +53,8 @@ const updateFunc = async (req, res) => {
     const combinedString = `${data.title}-${data.capacity_id}-${data.category_id}`;
     const slug = slugify(combinedString, { lower: true, strict: true, replacement: '-' });
 
-    const { title, product_id, capacity_id, color_id, stock, discount, price, desc, category_id } = data;
-    await product.update({ title, product_id, capacity_id, color_id, stock, discount, price, desc, category_id, slug });
+    const { title, capacity_id, color_id, stock, discount, price, desc, category_id } = data;
+    await product.update({ title, capacity_id, color_id, stock, discount, price, desc, category_id, slug });
 
     return res.status(200).json({ message: "Update product success", code: 0 });
 
