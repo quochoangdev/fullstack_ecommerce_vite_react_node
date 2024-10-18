60df701420608bdf0967cbd26e98891b04cc40d6
quochoangdev <quochoangdev.official@gmail.com>
2024-10-18 18:26:28 +0700

    update sidebar admin

--- client/src/admin/layout/AdminLayout/AdminLayout.jsx
+++ client/src/admin/layout/AdminLayout/AdminLayout.jsx
@@ -2,14 +2,18 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
 
+import classNames from 'classnames/bind'
+import styles from './AdminLayout.module.scss'
+
+const cx = classNames.bind(styles)
 
 const AdminLayout = ({ children }) => {
   return (
     <div>
       <Header />
       <div className='row'>
-        <div className='col-2'>
-          <div className=''><Sidebar /></div>
+        <div className={cx('sidebar', 'col-2')}>
+          <Sidebar />
         </div>
         <div className='col-10 d-flex flex-column'>
           <div >{children}</div>
--- client/src/admin/layout/components/Sidebar/Sidebar.jsx
+++ client/src/admin/layout/components/Sidebar/Sidebar.jsx
@@ -1,59 +1,38 @@
 import classNames from 'classnames/bind'
 import styles from './Sidebar.module.scss'
 import './Sidebar.css'
+import SidebarItem from './SidebarItem'
 const cx = classNames.bind(styles)
+
+import { MdOutlineDashboardCustomize } from 'react-icons/md'
+import { CgProfile } from 'react-icons/cg'
 
 const Sidebar = () => {
   return (
     <div className={cx('sidebar')}>
-      <div className="accordion accordion-flush" id="sidebar">
+      <div className="sidebar-label ms-3">Account</div>
+      <SidebarItem
+        title='Home'
+        rank={1}
+        icon={<MdOutlineDashboardCustomize className="accordion-icon" />}
+      />
+      <SidebarItem
+        title='Profile'
+        rank={2}
+        icon={<CgProfile className="accordion-icon" />}
+      />
 
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button className="accordion-button collapsed gl-gb-cl-admin" type="button" data-bs-toggle="collapse" data-bs-target="#one" aria-expanded="false" aria-controls="one">
-              Home
-            </button>
-          </h2>
-          <div id="one" className="accordion-collapse collapse gl-gb-cl-admin" data-bs-parent="#sidebar">
-            <div className="accordion-body">Placeholder</div>
-            {/* Bắt đầu cấp con */}
-            <div className="accordion accordion-flush" id="oneAccordion">
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button className="accordion-button collapsed gl-gb-cl-admin" type="button" data-bs-toggle="collapse" data-bs-target="#one-one" aria-expanded="false" aria-controls="one-one">
-                    one Accordion Item #1
-                  </button>
-                </h2>
-                <div id="one-one" className="accordion-collapse collapse" data-bs-parent="#oneAccordion">
-                  <div className="accordion-body gl-gb-cl-admin">one Placeholder #1</div>
-                </div>
-              </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button className="accordion-button collapsed gl-gb-cl-admin" type="button" data-bs-toggle="collapse" data-bs-target="#one-two" aria-expanded="false" aria-controls="one-two">
-                    one Accordion Item #2
-                  </button>
-                </h2>
-                <div id="one-two" className="accordion-collapse collapse" data-bs-parent="#oneAccordion">
-                  <div className="accordion-body gl-gb-cl-admin">one Placeholder #2</div>
-                </div>
-              </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button className="accordion-button collapsed gl-gb-cl-admin" type="button" data-bs-toggle="collapse" data-bs-target="#one-three" aria-expanded="false" aria-controls="one-three">
-                    one Accordion Item #3
-                  </button>
-                </h2>
-                <div id="one-three" className="accordion-collapse collapse" data-bs-parent="#oneAccordion">
-                  <div className="accordion-body gl-gb-cl-admin">one Placeholder #2</div>
-                </div>
-              </div>
-            </div>
-            {/* Kết thúc cấp con */}
-          </div>
-        </div>
-
-      </div>
+      <div className="sidebar-label ms-3">Product</div>
+      <SidebarItem
+        title='Product'
+        rank={3}
+        icon={<CgProfile className="accordion-icon" />}
+      />
+      <SidebarItem
+        title='Profile'
+        rank={4}
+        icon={<CgProfile className="accordion-icon" />}
+      />
     </div>
   )
 }
