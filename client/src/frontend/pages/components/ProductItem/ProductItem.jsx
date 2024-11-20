98b77b201b32e1d9a67ac09e49132a01bd329f1a
quochoangdev <quochoangdev.official@gmail.com>
2024-11-20 18:00:04 +0700

    seeder iphone

--- client/src/frontend/pages/components/ProductItem/ProductItem.jsx
+++ client/src/frontend/pages/components/ProductItem/ProductItem.jsx
@@ -24,7 +24,7 @@ const ProductItem = ({ stt }) => {
 
   // ---------- navigation ----------
   const limitPage = {
-    product: 12
+    product: 100
   }
 
   const handleFavoriteClick = (event) => {
@@ -109,75 +109,78 @@ const ProductItem = ({ stt }) => {
         <div className={cx('col-2', 'w-100', 'text-dark')}>
           <span>
             <div className={cx('row', 'd-flex', 'flex-wrap', 'grid', 'pb-3')}>
-              {products && products.map((item, index) => (
-                <div key={index} className={cx('col-2', 'pt-0', 'pb-2', 'px-1')}>
-                  <div className={cx('cs-list-item', 'bg-white', 'text-decoration-none', 'text-dark')}>
-                    <div className={cx('cs-item-block')}>
-                      <div className={cx('cs-card')}>
-                        <div className={cx('cs-item-pic')}>
-                          <img className={cx('cs-item-pic-content')} src={item?.configs[0]?.images[0]?.url || ''} alt='Product' />
-                        </div>
-                        <div className={cx('cs-card-body')}>
-                          <button
-                            type="button"
-                            data-bs-toggle="modal" data-bs-target={`#cartModalProd-${stt}`} onClick={() => handleRemoveBackdrop(item)}
-                            className={cx('cs-custom-btn', 'codepro-custom-btn', 'codepro-btn-3', 'me-2', 'text-decoration-none', 'text-white', 'text-center')}
-                          >
-                            ADD TO CART
-                          </button>
-                          <button
-                            onClick={(event) => {
-                              event.stopPropagation()
-                              window.location.href = config.routes.homeAdmin
-                            }}
-                            className={cx('cs-custom-btn', 'codepro-custom-btn', 'codepro-btn-3', 'text-decoration-none', 'text-white', 'text-center')}
-                          >
-                            QUICK VIEW
-                          </button>
-                        </div>
-                      </div>
-                      <a className={cx('text-decoration-none text-dark', 'cs-item-desc')} href={`/${item?.slug}`} >
-                        <div className={cx('cs-item-desc-title')}>
-                          <div className={cx('cs-item-desc-content')}>⚡️ Giá Sốc ⚡️ {item?.Brand?.name} {item?.Version?.name} {item?.Capacity?.name} {item?.configs[0]?.Color?.name}</div>
-                        </div>
-                        <div className={cx('cs-item-desc-voucher', 'd-flex')}>
-                          <div className={cx('cs-voucher')}>Rẻ Vô Địch</div>
-                          <div className={cx('cs-voucher')}>#ShopXuHuong</div>
-                        </div>
-                        <div className={cx('cs-item-desc-price')}>
-                          <div className={cx('cs-item-desc-price-sale')}>
-                            <span className={cx('cs-unit')}>₫</span> <span className={cx('cs-price')}>{item?.configs[0]?.price}</span>
+              {products && products.map((item, index) => {
+                const randomConfig = Math.floor(Math.random() * (item?.configs.length))
+                return (
+                  <div key={index} className={cx('col-2', 'pt-0', 'pb-2', 'px-1')}>
+                    <div className={cx('cs-list-item', 'bg-white', 'text-decoration-none', 'text-dark')}>
+                      <div className={cx('cs-item-block')}>
+                        <div className={cx('cs-card')}>
+                          <div className={cx('cs-item-pic')}>
+                            <img className={cx('cs-item-pic-content')} src={item?.configs[randomConfig]?.images[0]?.url || ''} alt='Product' />
                           </div>
-                          <div className={cx('cs-item-desc-price-origin')}>₫{item?.configs[0]?.price}</div>
-                          <div className={cx('cs-item-desc-price-percent')}>
-                            <div className={cx('cs-box-percent')}>
-                              <span className={cx('cs-content-percent')}>-{item?.configs[0]?.discount}%</span>
+                          <div className={cx('cs-card-body')}>
+                            <button
+                              type="button"
+                              data-bs-toggle="modal" data-bs-target={`#cartModalProd-${stt}`} onClick={() => handleRemoveBackdrop(item)}
+                              className={cx('cs-custom-btn', 'codepro-custom-btn', 'codepro-btn-3', 'me-2', 'text-decoration-none', 'text-white', 'text-center')}
+                            >
+                              ADD TO CART
+                            </button>
+                            <button
+                              onClick={(event) => {
+                                event.stopPropagation()
+                                window.location.href = config.routes.homeAdmin
+                              }}
+                              className={cx('cs-custom-btn', 'codepro-custom-btn', 'codepro-btn-3', 'text-decoration-none', 'text-white', 'text-center')}
+                            >
+                              QUICK VIEW
+                            </button>
+                          </div>
+                        </div>
+                        <a className={cx('text-decoration-none text-dark', 'cs-item-desc')} href={`/${item?.slug}`} >
+                          <div className={cx('cs-item-desc-title')}>
+                            <div className={cx('cs-item-desc-content')}>⚡️ Giá Sốc ⚡️ {item?.Brand?.name} {item?.Version?.name} {item?.Capacity?.name} {item?.configs[randomConfig]?.Color?.name}</div>
+                          </div>
+                          <div className={cx('cs-item-desc-voucher', 'd-flex')}>
+                            <div className={cx('cs-voucher')}>Rẻ Vô Địch</div>
+                            <div className={cx('cs-voucher')}>#ShopXuHuong</div>
+                          </div>
+                          <div className={cx('cs-item-desc-price')}>
+                            <div className={cx('cs-item-desc-price-sale')}>
+                              <span className={cx('cs-unit')}>₫</span> <span className={cx('cs-price')}>{item?.configs[randomConfig]?.price}</span>
+                            </div>
+                            <div className={cx('cs-item-desc-price-origin')}>₫{item?.configs[randomConfig]?.price}</div>
+                            <div className={cx('cs-item-desc-price-percent')}>
+                              <div className={cx('cs-box-percent')}>
+                                <span className={cx('cs-content-percent')}>-{item?.configs[randomConfig]?.discount}%</span>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                        <div className={cx('cs-item-rating', 'd-flex', 'align-items-center')}>
-                          <div className={cx('cs-item-rating-start', 'me-2')}>
-                            <Rating name="size-small" value={4} size="small" readOnly />
+                          <div className={cx('cs-item-rating', 'd-flex', 'align-items-center')}>
+                            <div className={cx('cs-item-rating-start', 'me-2')}>
+                              <Rating name="size-small" value={4} size="small" readOnly />
+                            </div>
+                            <div className={cx('cs-item-sell')}>Đã bán 366,2k</div>
                           </div>
-                          <div className={cx('cs-item-sell')}>Đã bán 366,2k</div>
-                        </div>
-                        <div className={cx('cs-item-located', 'd-flex', 'align-items-center', 'justify-content-between')}>
-                          <div className={cx('cs-destination')}>TP. Hồ Chí Minh</div>
-                          <div>
-                            <Checkbox {...label} onClick={handleFavoriteClick} icon={<FavoriteBorder />} checkedIcon={<Favorite />} sx={{ '& .MuiSvgIcon-root': { fontSize: 14 } }} />
+                          <div className={cx('cs-item-located', 'd-flex', 'align-items-center', 'justify-content-between')}>
+                            <div className={cx('cs-destination')}>TP. Hồ Chí Minh</div>
+                            <div>
+                              <Checkbox {...label} onClick={handleFavoriteClick} icon={<FavoriteBorder />} checkedIcon={<Favorite />} sx={{ '& .MuiSvgIcon-root': { fontSize: 14 } }} />
+                            </div>
                           </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div className={cx('product__price--percent')}>
-                      <img className={cx('product__price--percent')} src='https://res.cloudinary.com/dqhj1sukr/image/upload/v1730468046/uploadLocal_ecommerce/azxoe0ipn6yl0hifhdhz.png' />
-                      <p className={cx('product__price--percent-detail')}>
-                        Giảm&nbsp;{item?.configs[0]?.discount}%
-                      </p>
+                        </a>
+                      </div>
+                      <div className={cx('product__price--percent')}>
+                        <img className={cx('product__price--percent')} src='https://res.cloudinary.com/dqhj1sukr/image/upload/v1730468046/uploadLocal_ecommerce/azxoe0ipn6yl0hifhdhz.png' />
+                        <p className={cx('product__price--percent-detail')}>
+                          Giảm&nbsp;{item?.configs[randomConfig]?.discount}%
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                )
+              })}
             </div>
           </span>
         </div>
--- server/src/seeders/productsData/smartphone/iphone.js
+++ server/src/seeders/productsData/smartphone/iphone.js
@@ -16,18 +16,125 @@ module.exports = [
   { id: 6, title: 'Điện thoại iPhone 12 - RAM 4GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 2, ram_id: 4, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-12-4gb-256gb' },
 
   // ---------- iphone 12 pro 128GB ----------
-  { id: 7, title: 'Điện thoại iPhone 12 Pro - RAM 4GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 3, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-12-pro-4gb-128gb' },
+  { id: 7, title: 'Điện thoại iPhone 12 Pro - RAM 6GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 3, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-12-pro-6gb-128gb' },
   // ---------- iphone 12 pro 256GB ----------
-  { id: 8, title: 'Điện thoại iPhone 12 Pro - RAM 4GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 3, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-12-pro-4gb-256gb' },
+  { id: 8, title: 'Điện thoại iPhone 12 Pro - RAM 6GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 3, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-12-pro-6gb-256gb' },
   // ---------- iphone 12 pro 512GB ----------
-  { id: 9, title: 'Điện thoại iPhone 12 Pro - RAM 4GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 3, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-12-pro-4gb-512gb' },
+  { id: 9, title: 'Điện thoại iPhone 12 Pro - RAM 6GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 3, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-12-pro-6gb-512gb' },
 
   // ---------- iphone 12 pro max 128GB ----------
-  { id: 10, title: 'Điện thoại iPhone 12 Pro Max - RAM 4GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 4, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-12-pro-max-4gb-128gb' },
+  { id: 10, title: 'Điện thoại iPhone 12 Pro Max - RAM 6GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 4, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-12-pro-max-6gb-128gb' },
   // ---------- iphone 12 pro max 256GB ----------
-  { id: 11, title: 'Điện thoại iPhone 12 Pro Max - RAM 4GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 4, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-12-pro-max-4gb-256gb' },
+  { id: 11, title: 'Điện thoại iPhone 12 Pro Max - RAM 6GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 4, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-12-pro-max-6gb-256gb' },
   // ---------- iphone 12 pro max 512GB ----------
-  { id: 12, title: 'Điện thoại iPhone 12 Pro Max - RAM 4GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 4, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-12-pro-max-4gb-512gb' },
+  { id: 12, title: 'Điện thoại iPhone 12 Pro Max - RAM 6GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 4, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-12-pro-max-6gb-512gb' },
+
+  // ---------- iphone 13 128GB ----------
+  { id: 13, title: 'Điện thoại iPhone 13 - RAM 4GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 5, ram_id: 4, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-13-4gb-128gb' },
+  // ---------- iphone 13 256GB ----------
+  { id: 14, title: 'Điện thoại iPhone 13 - RAM 4GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 5, ram_id: 4, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-13-4gb-256gb' },
+  // ---------- iphone 13 512GB ----------
+  { id: 15, title: 'Điện thoại iPhone 13 - RAM 4GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 5, ram_id: 4, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-13-4gb-512gb' },
+
+  // ---------- iphone 13 pro 128GB ----------
+  { id: 16, title: 'Điện thoại iPhone 13 Pro - RAM 6GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 6, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-13-pro-6gb-128gb' },
+  // ---------- iphone 13 pro 256GB ----------
+  { id: 17, title: 'Điện thoại iPhone 13 Pro - RAM 6GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 6, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-13-pro-6gb-256gb' },
+  // ---------- iphone 13 pro 512GB ----------
+  { id: 18, title: 'Điện thoại iPhone 13 Pro - RAM 6GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 6, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-13-pro-6gb-512gb' },
+
+  // ---------- iphone 13 pro max 128GB ----------
+  { id: 19, title: 'Điện thoại iPhone 13 Pro Max - RAM 6GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 7, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-13-pro-max-6gb-128gb' },
+  // ---------- iphone 13 pro max 256GB ----------
+  { id: 20, title: 'Điện thoại iPhone 13 Pro Max - RAM 6GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 7, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-13-pro-max-6gb-256gb' },
+  // ---------- iphone 13 pro max 512GB ----------
+  { id: 21, title: 'Điện thoại iPhone 13 Pro Max - RAM 6GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 7, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-13-pro-max-6gb-512gb' },
+
+  // ---------- iphone 14 128GB ----------
+  { id: 22, title: 'Điện thoại iPhone 14 - RAM 4GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 8, ram_id: 4, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-14-4gb-128gb' },
+  // ---------- iphone 14 256GB ----------
+  { id: 23, title: 'Điện thoại iPhone 14 - RAM 4GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 8, ram_id: 4, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-14-4gb-256gb' },
+  // ---------- iphone 14 512GB ----------
+  { id: 24, title: 'Điện thoại iPhone 14 - RAM 4GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 8, ram_id: 4, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-14-4gb-512gb' },
+
+  // ---------- iphone 14 pro 128GB ----------
+  { id: 25, title: 'Điện thoại iPhone 14 Pro - RAM 6GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 9, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-14-pro-6gb-128gb' },
+  // ---------- iphone 14 pro 256GB ----------
+  { id: 26, title: 'Điện thoại iPhone 14 Pro - RAM 6GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 9, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-14-pro-6gb-256gb' },
+  // ---------- iphone 14 pro 512GB ----------
+  { id: 27, title: 'Điện thoại iPhone 14 Pro - RAM 6GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 9, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-14-pro-6gb-512gb' },
+
+  // ---------- iphone 14 pro max 128GB ----------
+  { id: 28, title: 'Điện thoại iPhone 14 Pro Max - RAM 6GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 10, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-14-pro-max-6gb-128gb' },
+  // ---------- iphone 14 pro max 256GB ----------
+  { id: 29, title: 'Điện thoại iPhone 14 Pro Max - RAM 6GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 10, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-14-pro-max-6gb-256gb' },
+  // ---------- iphone 14 pro max 512GB ----------
+  { id: 30, title: 'Điện thoại iPhone 14 Pro Max - RAM 6GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 10, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-14-pro-max-6gb-512gb' },
+
+  // ---------- iphone 15 128GB ----------
+  { id: 31, title: 'Điện thoại iPhone 15 - RAM 6GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 11, ram_id: 5, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-15-6gb-128gb' },
+  // ---------- iphone 15 256GB ----------
+  { id: 32, title: 'Điện thoại iPhone 15 - RAM 6GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 11, ram_id: 5, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-15-6gb-256gb' },
+  // ---------- iphone 15 512GB ----------
+  { id: 33, title: 'Điện thoại iPhone 15 - RAM 6GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 11, ram_id: 5, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-15-6gb-512gb' },
+  // ---------- iphone 15 1TB ----------
+  { id: 34, title: 'Điện thoại iPhone 15 - RAM 6GB, Dung lượng bộ nhớ 1TB', category_id: 1, desc: null, brand_id: 1, version_id: 11, ram_id: 5, capacity_id: 8, is_active: 1, slug: 'dien-thoai-iphone-15-6gb-1tb' },
+
+  // ---------- iphone 15 pro 128GB ----------
+  { id: 35, title: 'Điện thoại iPhone 15 Pro - RAM 8GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 12, ram_id: 6, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-15-pro-8gb-128gb' },
+  // ---------- iphone 15 pro 256GB ----------
+  { id: 36, title: 'Điện thoại iPhone 15 Pro - RAM 8GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 12, ram_id: 6, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-15-pro-8gb-256gb' },
+  // ---------- iphone 15 pro 512GB ----------
+  { id: 37, title: 'Điện thoại iPhone 15 Pro - RAM 8GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 12, ram_id: 6, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-15-pro-8gb-512gb' },
+  // ---------- iphone 15 pro 1TB ----------
+  { id: 38, title: 'Điện thoại iPhone 15 Pro - RAM 8GB, Dung lượng bộ nhớ 1TB', category_id: 1, desc: null, brand_id: 1, version_id: 12, ram_id: 6, capacity_id: 8, is_active: 1, slug: 'dien-thoai-iphone-15-pro-8gb-1tb' },
+
+  // ---------- iphone 15 pro max 128GB ----------
+  { id: 39, title: 'Điện thoại iPhone 15 Pro Max - RAM 8GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 13, ram_id: 6, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-15-pro-max-8gb-128gb' },
+  // ---------- iphone 15 pro max 256GB ----------
+  { id: 40, title: 'Điện thoại iPhone 15 Pro Max - RAM 8GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 13, ram_id: 6, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-15-pro-max-8gb-256gb' },
+  // ---------- iphone 15 pro max 512GB ----------
+  { id: 41, title: 'Điện thoại iPhone 15 Pro Max - RAM 8GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 13, ram_id: 6, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-15-pro-max-8gb-512gb' },
+  // ---------- iphone 15 pro max 1TB ----------
+  { id: 42, title: 'Điện thoại iPhone 15 Pro Max - RAM 8GB, Dung lượng bộ nhớ 1TB', category_id: 1, desc: null, brand_id: 1, version_id: 13, ram_id: 6, capacity_id: 8, is_active: 1, slug: 'dien-thoai-iphone-15-pro-max-8gb-1tb' },
+
+  // ---------- iphone 16 128GB ----------
+  { id: 43, title: 'Điện thoại iPhone 16 - RAM 8GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 14, ram_id: 6, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-16-8gb-128gb' },
+  // ---------- iphone 16 256GB ----------
+  { id: 44, title: 'Điện thoại iPhone 16 - RAM 8GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 14, ram_id: 6, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-16-8gb-256gb' },
+  // ---------- iphone 16 512GB ----------
+  { id: 45, title: 'Điện thoại iPhone 16 - RAM 8GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 14, ram_id: 6, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-16-8gb-512gb' },
+  // ---------- iphone 16 1TB ----------
+  { id: 46, title: 'Điện thoại iPhone 16 - RAM 8GB, Dung lượng bộ nhớ 1TB', category_id: 1, desc: null, brand_id: 1, version_id: 14, ram_id: 6, capacity_id: 8, is_active: 1, slug: 'dien-thoai-iphone-16-8gb-1tb' },
+
+  // ---------- iphone 16 pro 128GB ----------
+  { id: 47, title: 'Điện thoại iPhone 16 Pro - RAM 8GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 15, ram_id: 6, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-16-pro-8gb-128gb' },
+  // ---------- iphone 16 pro 256GB ----------
+  { id: 48, title: 'Điện thoại iPhone 16 Pro - RAM 8GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 15, ram_id: 6, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-16-pro-8gb-256gb' },
+  // ---------- iphone 16 pro 512GB ----------
+  { id: 49, title: 'Điện thoại iPhone 16 Pro - RAM 8GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 15, ram_id: 6, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-16-pro-8gb-512gb' },
+  // ---------- iphone 16 pro 1TB ----------
+  { id: 50, title: 'Điện thoại iPhone 16 Pro - RAM 8GB, Dung lượng bộ nhớ 1TB', category_id: 1, desc: null, brand_id: 1, version_id: 15, ram_id: 6, capacity_id: 8, is_active: 1, slug: 'dien-thoai-iphone-16-pro-8gb-1tb' },
+
+  // ---------- iphone 16 pro max 128GB ----------
+  { id: 51, title: 'Điện thoại iPhone 16 Pro Max - RAM 8GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 16, ram_id: 6, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-16-pro-max-8gb-128gb' },
+  // ---------- iphone 16 pro max 256GB ----------
+  { id: 52, title: 'Điện thoại iPhone 16 Pro Max - RAM 8GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 16, ram_id: 6, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-16-pro-max-8gb-256gb' },
+  // ---------- iphone 16 pro max 512GB ----------
+  { id: 53, title: 'Điện thoại iPhone 16 Pro Max - RAM 8GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 16, ram_id: 6, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-16-pro-max-8gb-512gb' },
+  // ---------- iphone 16 pro max 1TB ----------
+  { id: 54, title: 'Điện thoại iPhone 16 Pro Max - RAM 8GB, Dung lượng bộ nhớ 1TB', category_id: 1, desc: null, brand_id: 1, version_id: 16, ram_id: 6, capacity_id: 8, is_active: 1, slug: 'dien-thoai-iphone-16-pro-max-8gb-1tb' },
+
+  // ---------- iphone 16 plus 128GB ----------
+  { id: 55, title: 'Điện thoại iPhone 16 Plus - RAM 8GB, Dung lượng bộ nhớ 128GB', category_id: 1, desc: null, brand_id: 1, version_id: 17, ram_id: 6, capacity_id: 5, is_active: 1, slug: 'dien-thoai-iphone-16-plus-8gb-128gb' },
+  // ---------- iphone 16 plus 256GB ----------
+  { id: 56, title: 'Điện thoại iPhone 16 Plus - RAM 8GB, Dung lượng bộ nhớ 256GB', category_id: 1, desc: null, brand_id: 1, version_id: 17, ram_id: 6, capacity_id: 6, is_active: 1, slug: 'dien-thoai-iphone-16-plus-8gb-256gb' },
+  // ---------- iphone 16 plus 512GB ----------
+  { id: 57, title: 'Điện thoại iPhone 16 Plus - RAM 8GB, Dung lượng bộ nhớ 512GB', category_id: 1, desc: null, brand_id: 1, version_id: 17, ram_id: 6, capacity_id: 7, is_active: 1, slug: 'dien-thoai-iphone-16-plus-8gb-512gb' },
+  // ---------- iphone 16 plus 1TB ----------
+  { id: 58, title: 'Điện thoại iPhone 16 Plus - RAM 8GB, Dung lượng bộ nhớ 1TB', category_id: 1, desc: null, brand_id: 1, version_id: 17, ram_id: 6, capacity_id: 8, is_active: 1, slug: 'dien-thoai-iphone-16-plus-8gb-1tb' },
+
+
 ]
 
 
